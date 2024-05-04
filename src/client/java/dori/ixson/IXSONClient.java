4a88bb80fe309221fe52f325639b5876a501215b
DoriGAMES <doricoding@gmail.com>
2024-05-04 20:22:18 +0200

    init

---
+++ src/client/java/dori/ixson/IXSONClient.java
@@ -0,0 +1,10 @@
+package dori.ixson;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IXSONClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
